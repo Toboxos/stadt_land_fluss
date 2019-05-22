8c187254a8015ee8e352d1a379c32404d65a1a65
duzilia <felicia.schmidt@gmx.de>
2019-05-22 13:03:08 +0200

    Feli ist toll

---
+++ CLogik.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdint.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class CLogik {
+public:
+	CLogik() {};
+	~CLogik() {};
+
+	void createPlayer();
+	void awardPoints();
+	string getWinner();
+	
+private:
+	vector<CSpieler> players;
+};
