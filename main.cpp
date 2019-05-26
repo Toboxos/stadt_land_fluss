9fe4c1893d8794422576a7e6bb39025182f9f3c5
Tobias Bungard <tobi-bungard@t-online.de>
2019-05-26 20:59:28 +0200

    Added network example

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <QApplication>
 #include "spielstart.h"
 
+#include "Examples/networkexample.h"
 
 std::vector<Spieler> _spielerListe;
 Spieleinstellungen  *_einstellung = new Spieleinstellungen();
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    NetworkExample e;
+
     SpielStart w;
 
     w.exec();
+
 
 
     return a.exec();
