2b05178210d51c84e2aaa81f403bcef67df11964
noahkel <noahkeltsch@gmx.de>
2019-05-28 11:04:06 +0200

    Update von hauptspielfenster.cpp, include von spielerliste, damit einstellung gefunden wird

--- timer.cpp
+++ timer.cpp
@@ -1,16 +1,31 @@
 #include "timer.h"
 #include <QtCore>
 #include <QtDebug>
+#include <spielerliste.h>
 
+///<summary> constructor of timer class </summary>
+/// <parameters> </parameters>
 timer::timer()
 {
     InternalTimer = new QTimer(this);
     connect(InternalTimer, SIGNAL(timeout()), this, SLOT(slot()));
-    InternalTimer->start(1000);
 
 }
+///<summary> destructor of timer class</summary>
+/// <parameters></parameters>
+timer::~timer()
+{
+    delete InternalTimer;
+}
 
-void timer::slot()
+///<summary> is called on Round end, when a Player completed his words </summary>
+///<parameters> </parameters>
+void timer::endRound(){
+    InternalTimer->singleShot(_einstellung->getCountdown()*1000, this, SLOT(roundEnd()));
+}
+///<summary> slot that executes when timer runs out</summary>
+/// <parameters> </parameters>
+void timer::roundEndSlot()
 {
 
     qDebug() << "Timer executed";
