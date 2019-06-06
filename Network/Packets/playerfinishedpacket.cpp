aa259ad9fc708f9a5e14c1a21205042fd7e5e2a7
Tobias Bungard <tobi-bungard@t-online.de>
2019-06-06 08:57:30 +0200

    Added player name to PlayerFinishedPacket

--- Network/Packets/playerfinishedpacket.cpp
+++ Network/Packets/playerfinishedpacket.cpp
@@ -1,12 +1,26 @@
 #include "playerfinishedpacket.h"
 
-PlayerFinishedPacket::PlayerFinishedPacket()
-{
+PlayerFinishedPacket::PlayerFinishedPacket() {
     m_type = PLAYER_FINISHED_PACKET;
 }
 
+PlayerFinishedPacket::PlayerFinishedPacket(QString name) {
+    m_type = PLAYER_FINISHED_PACKET;
+    m_name = name;
+}
+
+void PlayerFinishedPacket::setName(QString name) {
+    m_name = name;
+}
+
+QString PlayerFinishedPacket::getName() {
+    return m_name;
+}
+
 void PlayerFinishedPacket::writeData(QTcpSocket& socket) {
+    writeQString(socket, m_name);
 }
 
 void PlayerFinishedPacket::readData(QTcpSocket& socket) {
+    readQString(socket, m_name);
 }
--- Network/Packets/playerfinishedpacket.h
+++ Network/Packets/playerfinishedpacket.h
@@ -2,14 +2,22 @@
 #define PLAYERFINISHEDPACKET_H
 #include "packet.h"
 
-class PlayerFinishedPacket : public Packet
-{
-public:
-    PlayerFinishedPacket();
+/**
+ * @brief Informing, that a player has sent his answers and is finished.
+ */
+class PlayerFinishedPacket : public Packet {
+    public:
+        PlayerFinishedPacket();
+        PlayerFinishedPacket(QString name);
 
-    void readData(QTcpSocket& socket);
-    void writeData(QTcpSocket& socket);
+        void setName(QString name);
+        QString getName();
 
+        void readData(QTcpSocket& socket);
+        void writeData(QTcpSocket& socket);
+
+    private:
+        QString m_name; /**< Name of player who has finished */
 };
 
 #endif // PLAYERFINISHEDPACKET_H
