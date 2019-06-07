014f0fc9ddf8deaacd6c7e1f10bab18276024b1c
duzilia <felicia.schmidt@gmx.de>
2019-06-07 11:40:03 +0200

    kommentare spieler-Klasse

--- spieler.cpp
+++ spieler.cpp
@@ -12,13 +12,11 @@ Spieler::Spieler(QString name)
    _nextId ++;
 }
 
-//gibt den Spielernamen zurück
 QString Spieler::getName()
 {
     return _name;
 }
 
-//gibt die Spieler ID zurück
 int Spieler::getId()
 {
     return _id;
@@ -30,51 +28,41 @@ void Spieler::setConnectionId(unsigned int id){
 
 void Spieler::setAnswers(QVector<QString> antworten){
     answers = antworten;
-
-
 }
 
 unsigned int Spieler::getConnectionId(){
     return connectionId;
 }
 
-//gibt die Gesamtpunktzahl des Spielers zurück
 int Spieler::getPunkte()
 {
     return _punkte;
 }
 
-//setzt die Gesamtpunktzahl des Spielers
 void Spieler::setPunkte(int punkte)
 {
     _punkte = punkte;
 }
 
-//gibt die Anzahl der Kategorien zurück
 unsigned int Spieler::Categories(){
     unsigned int c = answers.size();
     return c;
 }
 
-
-//gibt die Antworten des Spielers in der Runde zurück
 QString Spieler::getAnswer(unsigned int i){
     QString p = answers.at(i);
     return p;
 }
 
-//setzt die Antwort in der Runde für Kategorie Nr i
 void Spieler::setAnswer(unsigned int i, QString a){
     answers[i]=a;
 }
 
-//gibt die Punkte des Spielers in der Runde zurück
 QVector<int> Spieler::getCredit(){
     QVector<int> p = credits;
     return p;
 }
 
-//setzt die Punkte der Runde für Kategorie Nr i
 void Spieler::setCredit(unsigned int i, int j){
     credits[i]=j;
 }
