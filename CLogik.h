1038cadc0bd7930d548c3670846825a6aaaf4d5b
duzilia <felicia.schmidt@gmx.de>
2019-06-09 19:01:55 +0200

    ein bisschen Doku

--- CLogik.h
+++ CLogik.h
@@ -9,7 +9,7 @@
 #include <ctype.h>
 
 // When unit testing, dont include original ServerSocket
-// indeed include MockServerSocket which simulate an ServerSocket
+// indeed include MockServerSocket which simulates a ServerSocket
 #ifdef UNIT_TEST
 #include "UnitTests/mock_classes/mockserversocket.h"
 typedef MockServerSocket ServerSocket;
@@ -63,8 +63,8 @@ public:
     void Punktevergabe();
 
     /**
-     * @brief sorts players by total credit!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-     * @return players organized by rank
+     * @brief sorts players by total credit, packs ranking by names and credit in EndGamePacket
+     * @return EndGamePacket
      */
     EndGamePacket getWinner();
 
@@ -85,26 +85,70 @@ public:
     /// \return
     ///
     QVector<Spieler>* getSpielerListe();
+
+    ///
+    /// \brief starts server socket
+    ///
     void starteServerSocket();
+
+    ///
+    /// \brief opens the game settings window
+    ///
     void openHostSpielEinstellungen();
+
+    ///
+    /// \brief opens window to set categories
+    ///
     void openKategorieEingabe();
+
+    ///
+    /// \brief opens player waiting room
+    ///
     void openSpielerWarteRaum();
+
+    ///
+    /// \brief sends a GameSettingsPacket and PlayerListPacket to every client
+    ///
     void sendeSpielStart();
+
+    ///
+    /// \brief sends a StartCountdownPacket to every client, sends EndGamePacket should the maximum number of rounds be achieved
+    ///
     void sendeRundenStart();
-    void endGame();    
+
+    ///
+    /// \brief endGame
+    ///
+    void endGame();
+
+    ///
+    /// \brief sends a packet to every client
+    /// \param packet that is to be sent
+    ///
     void sendToAll(Packet& p);
 public slots:
     /**
-     * @brief nimmt Signal mit Packet entgegen, erstellt neues Spielerobjekt und speichert die VerbindungsID ab
-     * @param packet
-     * @param id
+     * @brief triggered by signal playerJoined, creates new player object and saves connection id
+     * @param PlayerJoinPacket
+     * @param connection id client
      */
     void spieler_beitritt(PlayerJoinPacket packet, unsigned int id);
 	
+    ///
+    /// \brief saves the received answers from every player, triggers awarding of points
+    /// \param SendAnswersPacket
+    /// \param connection id
+    ///
     void bekommt_antwort(SendAnswersPacket packet, unsigned int id);
 
+    ///
+    /// \brief sends a RoundStartPacket with the round's letter to every player
+    ///
     void startInput();
 
+    ///
+    /// \brief playerFinished
+    ///
     void playerFinished();
 
     void endInput();
